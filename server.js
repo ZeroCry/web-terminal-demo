6316366cbca3dd1e9ab79a8e1f7001313bc11679
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-09-05 19:42:44 -0300

    listen on 3000 port

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ var app = express()
 app.get('/', function (req, res) {
     res.send('hello world')
 })
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+  
